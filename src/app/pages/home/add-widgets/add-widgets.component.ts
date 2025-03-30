b29daf34d10838cdf5a18019e725fa51b1191f28
Zetneki <zoltaizeteny0@gmail.com>
2025-03-30 16:55:39 +0200

    random push

---
+++ src/app/pages/home/add-widgets/add-widgets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-widgets',
+  imports: [],
+  templateUrl: './add-widgets.component.html',
+  styleUrl: './add-widgets.component.scss'
+})
+export class AddWidgetsComponent {
+
+}
